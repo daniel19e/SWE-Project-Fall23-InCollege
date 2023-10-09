0a8b04de26b3719eae281a08cb75c02c1be0cd44
Daniel <esc.daniel91@gmail.com>
2023-10-08 20:34:39 -0400

    show my network tests

--- social_test.py
+++ social_test.py
@@ -58,7 +58,7 @@ def test_connect_with_student_not_found(mock_search_first_and_last):
     assert result == False
 
 
-# (NEW TESTS)
+# (NEW TESTS) -------------------------
 @patch('social.db.search_students_by_criteria')
 def test_find_someone_i_know_lastname(mock_search_student_by_criteria, capsys):
     mock_search_student_by_criteria.return_value = [(
@@ -121,11 +121,63 @@ def test_find_someone_i_know_send_connection(mock_search_student_by_criteria, ca
     mock_search_student_by_criteria.return_value = [
         (1, 'testusername', 'testfirst', 'testlast', 'pass', 'major', 'univ', 'english', 1, 1, 1)]
     with patch('social.db.send_friend_request'), patch('builtins.input') as mock_input:
-            mock_input.side_effect = ['1', '0', '1']
-            social.find_someone_i_know('testuser1')
+        mock_input.side_effect = ['1', '0', '1']
+        social.find_someone_i_know('testuser1')
+        out, _ = capsys.readouterr()
+        assert "Connection request sent!" in out
+
+
+def test_show_my_network_go_back(capsys):
+    with patch('builtins.input') as mock_input, \
+            patch('social.db.get_pending_requests', return_value=[]), \
+            patch('social.db.get_connections', return_value=[]):
+        mock_input.side_effect = ['0']
+        social.show_my_network('testusername')
+        out, _ = capsys.readouterr()
+        assert out == "Your Network:\n\n\nMake your selection:\n1. Disconnect from someone\n0. Go back\n"
+
+
+def test_show_my_network_show_pending_requests_and_connections(capsys):
+    with patch('builtins.input') as mock_input, \
+            patch('social.db.get_pending_requests', return_value=[['pending1'], ['pending2'], ['pending3']]), \
+            patch('social.db.get_connections', return_value=['connection1', 'connection2', 'connection3']):
+        mock_input.side_effect = ['0']
+        social.show_my_network('testusername')
+        out, _ = capsys.readouterr()
+        for i in range(1, 4):
+            assert f"{i}. pending{i}" in out
+            assert f"{i}. connection{i}" in out
+
+
+def test_show_my_network_disconnect_from_someone_successfully(capsys):
+    with patch('builtins.input') as mock_input, \
+            patch('social.db.get_pending_requests', return_value=[['pending1'], ['pending2'], ['pending3']]), \
+            patch('social.db.get_connections', return_value=['connection1', 'connection2', 'connection3']):
+        mock_input.side_effect = ['1', '1', 'yes', '0']
+        social.show_my_network('testusername')
+        out, _ = capsys.readouterr()
+        assert "Select someone to disconnect from" in out
+        assert "Disconnected from connection1" in out
+
+
+def test_show_my_network_disconnect_from_someone_no_changes(capsys):
+    with patch('builtins.input') as mock_input, \
+            patch('social.db.get_pending_requests', return_value=[['pending1'], ['pending2'], ['pending3']]), \
+            patch('social.db.get_connections', return_value=['connection1', 'connection2', 'connection3']):
+        mock_input.side_effect = ['1', '1', 'no', '0']
+        social.show_my_network('testusername')
+        out, _ = capsys.readouterr()
+        assert "No changes were made" in out
+
+
+def test_show_my_network_accept_pending_request(capsys):
+    with patch('builtins.input') as mock_input, \
+            patch('social.db.get_pending_requests', return_value=[['pending1'], ['pending2'], ['pending3']]), \
+            patch('social.db.get_connections', return_value=['connection1', 'connection2', 'connection3']), \
+            patch('social.db.accept_friend_request', return_value=None):
+        for i in range(1, 4):
+            mock_input.side_effect = [f'a{i}']
+            social.show_my_network('testusername')
             out, _ = capsys.readouterr()
-            print("debug", out)
-            assert "Connection request sent!" in out
-            
-def test_show_my_network():
-    pass
+            assert f"You are now connected with pending{i}" in out # check we can accept all pending connections
+# (NEW TESTS) -------------------------
